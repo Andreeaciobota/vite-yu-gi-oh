b2d35261dc738ab611dcc799b3b39638de7bd143
Andreea Ciobota <135983461+Andreeaciobota@users.noreply.github.com>
2023-10-13 10:59:44 +0100

    [Add]:title

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<script>
+
+export default {
+    name: "AppHeader"
+}
+
+</script>
+
+<template>
+    <header>
+
+       
+        <!-- title -->
+
+        <div id="container">
+
+            <h1>YU-Gi-Oh</h1>
+            
+
+        </div>
+
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use "src/styles/general.scss" as *;
+@use "src/styles/partials/variables.scss" as *;
+
+
+header {
+    width: 100%;
+    height: 80px;
+    
+    #container {
+        @include container;
+    }
+}
+</style>
